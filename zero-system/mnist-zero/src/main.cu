e21052c405c66fc256e31f94fca241d814fa02a8
d0g0825 <danny.glass0@walmart.com>
2022-08-11 00:57:42 -0500

    mnist & misc

--- zero-system/mnist-zero/src/main.cu
+++ zero-system/mnist-zero/src/main.cu
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
-#include <core/mod.cuh>
+#include <nn/mod.cuh>
 
 int main(int argc, char **argv)
 {
 	printf("MNIST-ZERO");
-	auto arr = NdArray::rands(false, Shape(10, 10), 0.0f, 1.0f);
-	arr->print();
+
 	return 0;
 }
