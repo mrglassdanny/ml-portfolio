1be77a891d32c84ad15fab3a8961eed0728c0d7e
d0g0825 <danny.glass0@walmart.com>
2022-12-04 22:09:54 -0600

    matrix product rework

--- zero-system/zero-system/src/zero/nn/layer/matrix_product.cu
+++ zero-system/zero-system/src/zero/nn/layer/matrix_product.cu
@@ -27,45 +27,67 @@ __global__ void k_matrix_product_evaluate(float *in, float *w, float *out,
                 int in_idx = (batch_idx * channel_cnt * cnt) + (channel_idx * cnt) + (row_idx * col_cnt) + in_col_idx;
                 int w_idx = (filter_idx * channel_cnt * cnt) + (channel_idx * cnt) + (in_col_idx * col_cnt) + col_idx;
 
-                // atomicAdd(&out[out_elem_idx], (in[in_idx] * w[w_idx]));
-                out[out_elem_idx] += (in[in_idx] * w[w_idx]);
+                atomicAdd(&out[out_elem_idx], (in[in_idx] * w[w_idx]));
             }
         }
     }
 }
 
 __global__ void k_matrix_product_inc_param_derivatives(float *in, float *in_n, float *n, float *dw,
-                                                       int row_cnt, int col_cnt, int cnt)
+                                                       int row_cnt, int col_cnt, int cnt, int filter_cnt,
+                                                       int batch_size, int channel_cnt)
 {
-    int col_idx = blockIdx.x * blockDim.x + threadIdx.x;
-    int row_idx = blockIdx.y * blockDim.y + threadIdx.y;
+    int r_c_idx = blockIdx.x * blockDim.x + threadIdx.x;
+    int filter_idx = blockIdx.y * blockDim.y + threadIdx.y;
 
-    if (col_idx < col_cnt && row_idx < row_cnt)
+    int row_idx = r_c_idx / col_cnt;
+    int col_idx = r_c_idx % col_cnt;
+
+    if (col_idx < col_cnt && row_idx < row_cnt && filter_idx < filter_cnt)
     {
-        int w_elem_idx = row_idx * col_cnt + col_idx;
-        int n_idx = row_idx;
-        int in_idx = col_idx;
-
-        for (int in_col_idx = 0; in_col_idx < col_cnt; in_col_idx++)
+        for (int batch_idx = 0; batch_idx < batch_size; batch_idx++)
         {
-            dw[w_elem_idx] += (in[in_col_idx * col_cnt + in_idx] * n[in_col_idx * col_cnt + n_idx]);
+            for (int channel_idx = 0; channel_idx < channel_cnt; channel_idx++)
+            {
+                int w_elem_idx = (filter_idx * cnt) + (row_idx * col_cnt) + col_idx;
+
+                for (int in_col_idx = 0; in_col_idx < col_cnt; in_col_idx++)
+                {
+                    int n_idx = (batch_idx * channel_cnt * cnt) + (channel_idx * cnt) + (in_col_idx * col_cnt) + row_idx;
+                    int in_idx = (batch_idx * filter_cnt * cnt) + (filter_idx * cnt) + (in_col_idx * col_cnt) + col_idx;
+
+                    dw[w_elem_idx] += (in[in_idx] * n[n_idx]);
+                }
+            }
         }
     }
 }
 
-__global__ void k_matrix_product_agg_derivatives(float *in, float *w, float *out, int row_cnt, int col_cnt)
+__global__ void k_matrix_product_agg_derivatives(float *in, float *w, float *out, int row_cnt, int col_cnt, int cnt,
+                                                 int batch_size, int channel_cnt, int filter_cnt)
 {
-    int col_idx = blockIdx.x * blockDim.x + threadIdx.x;
-    int row_idx = blockIdx.y * blockDim.y + threadIdx.y;
+    int r_c_idx = blockIdx.x * blockDim.x + threadIdx.x;
+    int b_c_idx = blockIdx.y * blockDim.y + threadIdx.y;
 
-    if (col_idx < col_cnt && row_idx < row_cnt)
+    int batch_idx = b_c_idx / channel_cnt;
+    int channel_idx = b_c_idx % channel_cnt;
+
+    int row_idx = r_c_idx / col_cnt;
+    int col_idx = r_c_idx % col_cnt;
+
+    if (col_idx < col_cnt && row_idx < row_cnt && channel_idx < channel_cnt && batch_idx < batch_size)
     {
-        int out_elem_idx = row_idx * col_cnt + col_idx;
-        int w_row_idx = col_idx;
-
-        for (int in_col_idx = 0; in_col_idx < col_cnt; in_col_idx++)
+        for (int filter_idx = 0; filter_idx < filter_cnt; filter_idx++)
         {
-            out[out_elem_idx] += (in[row_idx * col_cnt + in_col_idx] * w[col_idx * col_cnt + in_col_idx]);
+            int out_elem_idx = (batch_idx * channel_cnt * cnt) + (channel_idx * cnt) + (row_idx * col_cnt) + col_idx;
+
+            for (int in_col_idx = 0; in_col_idx < col_cnt; in_col_idx++)
+            {
+                int in_idx = (batch_idx * filter_cnt * cnt) + (filter_idx * cnt) + (row_idx * col_cnt) + in_col_idx;
+                int w_idx = (filter_idx * channel_cnt * cnt) + (channel_idx * cnt) + (col_idx * col_cnt) + in_col_idx;
+
+                out[out_elem_idx] += (in[in_idx] * w[w_idx]);
+            }
         }
     }
 }
@@ -82,8 +104,8 @@ MatrixProduct::MatrixProduct(Shape in_shape, int filter_cnt, ActivationType acti
 
 void MatrixProduct::evaluate(Tensor *out)
 {
-    int grid_row_cnt = (this->batch_size() * this->channels() / ZERO_CORE_CUDA_THREADS_PER_BLOCK) + 1;
-    int grid_col_cnt = (this->rows() * this->cols() / ZERO_CORE_CUDA_THREADS_PER_BLOCK) + 1;
+    int grid_row_cnt = ((this->batch_size() * this->channels()) / ZERO_CORE_CUDA_THREADS_PER_BLOCK) + 1;
+    int grid_col_cnt = ((this->rows() * this->cols()) / ZERO_CORE_CUDA_THREADS_PER_BLOCK) + 1;
 
     dim3 grid_dims(grid_col_cnt, grid_row_cnt);
     dim3 block_dims(ZERO_CORE_CUDA_THREADS_PER_BLOCK, ZERO_CORE_CUDA_THREADS_PER_BLOCK);
@@ -94,28 +116,6 @@ void MatrixProduct::evaluate(Tensor *out)
     k_matrix_product_evaluate<<<grid_dims, block_dims>>>(n->data(), w->data(), out->data(),
                                                          this->rows(), this->cols(), this->rows() * this->cols(),
                                                          this->batch_size(), this->channels(), this->filters());
-
-    // int grid_row_cnt2 = (1 / ZERO_CORE_CUDA_THREADS_PER_BLOCK) + 1;
-    // int grid_col_cnt2 = (4 / ZERO_CORE_CUDA_THREADS_PER_BLOCK) + 1;
-    // dim3 grid_dims2(grid_col_cnt2, grid_row_cnt2);
-    // dim3 block_dims2(ZERO_CORE_CUDA_THREADS_PER_BLOCK, ZERO_CORE_CUDA_THREADS_PER_BLOCK);
-
-    // auto t1 = Tensor::ones(true, Shape(2, 2));
-    // auto t2 = Tensor::ones(true, Shape(2, 2));
-    // auto t3 = Tensor::zeros(true, Shape(2, 2));
-
-    // t1->set_val(0, 0.0f);
-    // t1->set_val(1, 4.0f);
-    // t1->set_val(2, 1.0f);
-    // t1->set_val(3, -3.0f);
-
-    // t2->set_val(0, 2.0f);
-    // t2->set_val(1, 3.0f);
-    // t2->set_val(2, 4.0f);
-    // t2->set_val(3, 5.0f);
-
-    // k_matrix_product_evaluate<<<grid_dims2, block_dims2>>>(t1->data(), t2->data(), t3->data(), 2, 2, 4, 1, 1, 1);
-    // t3->print();
 
     Activation::evaluate(out, this->batch_size(), this->out_features(), this->activation_);
 }
@@ -132,25 +132,27 @@ void MatrixProduct::derive(Tensor *in, Tensor *in_n)
     Activation::derive(in, in_n, this->batch_size(), this->out_features(), this->activation_);
 
     {
-        int grid_row_cnt = (this->rows() / ZERO_CORE_CUDA_THREADS_PER_BLOCK) + 1;
-        int grid_col_cnt = (this->cols() / ZERO_CORE_CUDA_THREADS_PER_BLOCK) + 1;
+        int grid_row_cnt = (this->filters() / ZERO_CORE_CUDA_THREADS_PER_BLOCK) + 1;
+        int grid_col_cnt = ((this->rows() * this->cols()) / ZERO_CORE_CUDA_THREADS_PER_BLOCK) + 1;
 
         dim3 grid_dims(grid_col_cnt, grid_row_cnt);
         dim3 block_dims(ZERO_CORE_CUDA_THREADS_PER_BLOCK, ZERO_CORE_CUDA_THREADS_PER_BLOCK);
 
         k_matrix_product_inc_param_derivatives<<<grid_dims, block_dims>>>(in->data(), in_n->data(), n->data(), dw->data(),
-                                                                          this->rows(), this->cols(), this->rows() * this->cols());
+                                                                          this->rows(), this->cols(), this->rows() * this->cols(),
+                                                                          this->filters(), this->batch_size(), this->channels());
     }
 
     {
-        int grid_row_cnt = (this->batch_size() / ZERO_CORE_CUDA_THREADS_PER_BLOCK) + 1;
-        int grid_col_cnt = (this->in_features() / ZERO_CORE_CUDA_THREADS_PER_BLOCK) + 1;
+        int grid_row_cnt = ((this->batch_size() * this->channels()) / ZERO_CORE_CUDA_THREADS_PER_BLOCK) + 1;
+        int grid_col_cnt = ((this->rows() * this->cols()) / ZERO_CORE_CUDA_THREADS_PER_BLOCK) + 1;
 
         dim3 grid_dims(grid_col_cnt, grid_row_cnt);
         dim3 block_dims(ZERO_CORE_CUDA_THREADS_PER_BLOCK, ZERO_CORE_CUDA_THREADS_PER_BLOCK);
 
         k_matrix_product_agg_derivatives<<<grid_dims, block_dims>>>(in->data(), w->data(), dn->data(),
-                                                                    this->rows(), this->cols());
+                                                                    this->rows(), this->cols(), this->rows() * this->cols(),
+                                                                    this->batch_size(), this->channels(), this->filters());
     }
 }
 
