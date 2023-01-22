c30a84c07d480c007b8b582fa3a525c3a29b46eb
d0g0825 <danny.glass0@walmart.com>
2023-01-21 21:51:21 -0600

    promo and move constructors

--- schneizel/schneizel/src/move.h
+++ schneizel/schneizel/src/move.h
@@ -12,6 +12,11 @@ namespace schneizel
         PieceType piecetyp;
         byte_t src_sqnum;
         byte_t dst_sqnum;
+        PieceType promo_piecetyp;
+
+        Move();
+        Move(PieceType piecetyp, byte_t src_sqnum, byte_t dst_sqnum);
+        Move(PieceType piecetyp, byte_t src_sqnum, byte_t dst_sqnum, PieceType promo_piecetyp);
     };
 
     struct MoveList
