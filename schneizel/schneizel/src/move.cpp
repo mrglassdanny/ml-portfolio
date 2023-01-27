030febd0ae7158d0c971c4ebfe4037ffac79a7a4
d0g0825 <danny.glass0@walmart.com>
2023-01-26 22:13:19 -0600

    remove last gives check remnants

--- schneizel/schneizel/src/move.cpp
+++ schneizel/schneizel/src/move.cpp
@@ -10,7 +10,6 @@ namespace schneizel
         this->src_sq = src_sq;
         this->dst_sq = dst_sq;
         this->promo_piecetyp = PieceType::None;
-        this->gives_check = false;
     }
 
     Move::Move(PieceType piecetyp, square_t src_sq, square_t dst_sq, PieceType promo_piecetyp)
@@ -19,24 +18,5 @@ namespace schneizel
         this->src_sq = src_sq;
         this->dst_sq = dst_sq;
         this->promo_piecetyp = promo_piecetyp;
-        this->gives_check = false;
-    }
-
-    Move::Move(PieceType piecetyp, square_t src_sq, square_t dst_sq, bool gives_check)
-    {
-        this->piecetyp = piecetyp;
-        this->src_sq = src_sq;
-        this->dst_sq = dst_sq;
-        this->promo_piecetyp = PieceType::None;
-        this->gives_check = gives_check;
-    }
-
-    Move::Move(PieceType piecetyp, square_t src_sq, square_t dst_sq, PieceType promo_piecetyp, bool gives_check)
-    {
-        this->piecetyp = piecetyp;
-        this->src_sq = src_sq;
-        this->dst_sq = dst_sq;
-        this->promo_piecetyp = promo_piecetyp;
-        this->gives_check = gives_check;
     }
 }
