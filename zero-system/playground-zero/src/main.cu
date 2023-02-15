f094797f4bf0239dd7e24cbcc829253834af3b06
d0g0825 <danny.glass0@walmart.com>
2023-02-15 17:06:02 -0600

    playground work

--- zero-system/playground-zero/src/main.cu
+++ zero-system/playground-zero/src/main.cu
@@ -6,32 +6,62 @@
 using namespace zero::core;
 using namespace zero::nn;
 
-// function: y = 0.25x + 2.95
+// function: y = 2x + 3
 
 int main(int argc, char **argv)
 {
-    auto x = Tensor::from_csv("data/x.csv");
-    auto y = Tensor::from_csv("data/y.csv");
-
-    x->reshape(Shape(x->count(), 1));
-    y->reshape(Shape(y->count(), 1));
-
-    x->print();
-    y->print();
-
-    auto model = new Model(new loss::MSE(), new Xavier());
-    model->linear(x->shape(), 1, nullptr);
-    model->set_optimizer(new optim::SGD(model->parameters(), 0.0000001f));
-
-    for (int epoch = 0; epoch < 50; epoch++)
+    // DATA SETUP:
+    auto train_x = Tensor::from_csv("data/train-x.csv");
+    auto train_y = Tensor::from_csv("data/train-y.csv");
     {
-        auto p = model->forward(x);
-        auto l = model->loss(p, y);
-        printf("LOSS: %f\n", l);
-        model->backward(p, y);
-        model->step();
+        train_x->reshape(Shape(train_x->count(), 1));
+        train_y->reshape(Shape(train_y->count(), 1));
     }
 
+    auto test_x = Tensor::from_csv("data/test-x.csv");
+    auto test_y = Tensor::from_csv("data/test-y.csv");
+    {
+        test_x->reshape(Shape(test_x->count(), 1));
+        test_y->reshape(Shape(test_y->count(), 1));
+    }
+
+    // MODEL SETUP:
+    float learning_rate = 0.01f;
+    auto model = new Model(new MSE(), new Xavier());
+    model->linear(train_x->shape(), 1, nullptr);
+    model->set_optimizer(new SGD(model->parameters(), learning_rate));
+
+    // TRAIN:
+    {
+        FILE *train_csv = fopen("temp/train.csv", "w");
+        fprintf(train_csv, "epoch,weight,bias,weight_derivative,bias_derivative,loss\n");
+        for (int epoch = 1; epoch <= 1000; epoch++)
+        {
+            auto p = model->forward(train_x);
+            auto l = model->loss(p, train_y);
+            printf("LOSS: %f\n", l);
+            model->backward(p, train_y);
+
+            fprintf(train_csv, "%d,%f,%f,%f,%f,%f\n", epoch, model->parameters()[0]->weights()->get_val(0), model->parameters()[0]->biases()->get_val(0),
+                    model->parameters()[0]->weight_gradients()->get_val(0) / model->batch_size() * learning_rate, model->parameters()[0]->bias_gradients()->get_val(0) / model->batch_size() * learning_rate, l);
+
+            model->step();
+
+            delete p;
+        }
+        fclose(train_csv);
+    }
+
+    // TEST:
+    {
+        model->change_batch_size(test_x->shape()[0]);
+        auto p = model->forward(test_x);
+        Tensor::to_csv("temp/test_p.csv", p);
+        delete p;
+    }
+
+    delete train_x;
+    delete train_y;
 
     return 0;
 }
