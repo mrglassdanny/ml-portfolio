e4501af4d2b9036693417695cb1fb92db1239fe4
mrglassdanny <mrglassdanny@gmail.com>
2023-02-20 19:48:15 -0600

    fix

--- schneizel/src/search.cpp
+++ schneizel/src/search.cpp
@@ -1229,7 +1229,7 @@ moves_loop: // When in check, search starts here
       // For PV nodes only, do a full PV search on the first move or after a fail
       // high (in the latter case search only if value < beta), otherwise let the
       // parent node fail low with value <= alpha and try another move.
-      if (PvNode && (moveCount == 1 || (value > alpha && (rootNode || value < beta))))
+      if (newDepth < MAX_PLY && PvNode && (moveCount == 1 || (value > alpha && (rootNode || value < beta))))
       {
           (ss+1)->pv = pv;
           (ss+1)->pv[0] = MOVE_NONE;
