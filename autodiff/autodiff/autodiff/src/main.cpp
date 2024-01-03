8437ae849b42ec6688c99b7cb2b60aec38926d9a
dglass-cellaware <daniel.glass@cellawaretech.com>
2024-01-02 21:44:27 -0600

    autodiff progress

--- autodiff/autodiff/autodiff/src/main.cpp
+++ autodiff/autodiff/autodiff/src/main.cpp
@@ -12,15 +12,32 @@ class Tensor
 private:
 	std::vector<int> shape;
 	float* data;
+	std::string fn;
 
 public:
 
-	// Example: {1, 2, 3, 4}
 	Tensor(std::vector<int> shape)
 	{
 		this->shape = shape;
-		this->data = (float*)malloc(sizeof(float) * this->num_elems());
+		this->data = (float*)malloc(sizeof(float) * this->size());
 		this->zeros();
+		this->fn = "N/A";
+	}
+
+	Tensor(std::vector<int> shape, float val)
+	{
+		this->shape = shape;
+		this->data = (float*)malloc(sizeof(float) * this->size());
+		this->fill(val);
+		this->fn = "N/A";
+	}
+
+	Tensor(std::vector<int> shape, float mean, float stddev)
+	{
+		this->shape = shape;
+		this->data = (float*)malloc(sizeof(float) * this->size());
+		this->random(mean, stddev);
+		this->fn = "N/A";
 	}
 
 	~Tensor()
@@ -28,9 +45,12 @@ public:
 		free(this->data);
 	}
 
-	void print()
+	void print(const char *nam)
 	{
-		for (int i = 0; i < this->shape.size(); i++)
+		printf("VARIABLE: %s\n", nam);
+
+		printf("SHAPE: ");
+		for (int i = 0; i < this->count(); i++)
 		{
 			if (i == this->shape.size() - 1)
 				printf("%d\n", this->shape[i]);
@@ -38,10 +58,11 @@ public:
 				printf("%dx", this->shape[i]);
 		}
 
-		switch (this->num_dims())
+		printf("DATA: ");
+		switch (this->shape.size())
 		{
 		case 1:
-			for (int i = 0; i < this->num_elems(); i++)
+			for (int i = 0; i < this->size(); i++)
 			{
 				printf("%f\t", this->data[i]);
 			}
@@ -57,20 +78,25 @@ public:
 			}
 			break;
 		default:
-			for (int i = 0; i < this->num_elems(); i++)
+			for (int i = 0; i < this->count(); i++)
 			{
 				printf("%f\t", this->data[i]);
 			}
 			break;
 		}
+
+		printf("\nFUNCTION: ");
+		printf(this->fn.c_str());
+
+		printf("\n\n");
 	}
 
-	int num_dims()
+	int count()
 	{
 		return this->shape.size();
 	}
 
-	int num_elems()
+	int size()
 	{
 		int num = 1;
 		for (auto dim : this->shape) 
@@ -82,29 +108,90 @@ public:
 
 	void zeros()
 	{
-		memset(this->data, 0, sizeof(float) * this->num_elems());
+		memset(this->data, 0, sizeof(float) * this->size());
 	}
 
-	void randoms(float mean, float stddev)
+	void fill(float val)
+	{
+		for (int i = 0; i < this->size(); i++)
+		{
+			this->data[i] = val;
+		}
+	}
+
+	void random(float mean, float stddev)
 	{
 		std::random_device rd;
 		std::mt19937 gen(rd());
 
-		for (int i = 0; i < this->num_elems(); i++)
+		for (int i = 0; i < this->size(); i++)
 		{
 			std::normal_distribution<float> d(mean, stddev);
 			this->data[i] = d(gen);
 		}
+	}
+
+	static Tensor* dot(Tensor* a, Tensor* b)
+	{
+		auto t = new Tensor({ 1 });
+		for (int i = 0; i < a->size(); i++)
+		{
+			t->data[0] += a->data[i] * b->data[i];
+		}
+		t->fn = "DOT PRODUCT";
+		return t;
+	}
+
+	static Tensor* sigmoid(Tensor *a)
+	{
+		auto t = new Tensor(a->shape);
+		for (int i = 0; i < a->size(); i++)
+		{
+			t->data[i] = (1.0f / (1.0f + exp(-(a->data[i]))));
+		}
+		t->fn = "SIGMOID";
+		return t;
+	}
+
+	static Tensor* mse(Tensor* p, Tensor* y)
+	{
+		auto t = new Tensor(p->shape);
+		for (int i = 0; i < p->size(); i++)
+		{
+			t->data[i] = (p->data[i] - y->data[i]) * (p->data[i] - y->data[i]);
+		}
+		t->fn = "MEAN SQUARED ERROR";
+		return t;
+	}
+
+	static Tensor* grad(Tensor *d)
+	{
+
 	}
 };
 
 
 int main(int argc, char **argv)
 {
-	auto t = new Tensor({5, 5});
-	t->print();
 
-	t->randoms(0.0f, 1.0f);
-	t->print();
+	auto x = new Tensor({ 5 }, 0, 1.0f);
+	auto y = new Tensor({ 1 }, 0, 1.0f);
+
+	auto w = new Tensor({ 5 }, 0, 0.01f);
+
+	x->print("x");
+	w->print("w");
+
+	auto z = Tensor::dot(x, w);
+	z->print("z");
+
+	auto h = Tensor::sigmoid(z);
+	h->print("h");
+
+	y->print("y");
+
+	auto l = Tensor::mse(h, y);
+	l->print("l");
+
 	return 0;
 }
