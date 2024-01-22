f67177a87d76b8405c79a70f22d49056ba026c9f
dglass-cellaware <daniel.glass@cellawaretech.com>
2024-01-21 20:59:25 -0600

    mnist testing

--- tallgeese/playground-tallgeese/src/main.cpp
+++ tallgeese/playground-tallgeese/src/main.cpp
@@ -9,29 +9,21 @@ using namespace tallgeese::nn;
 
 int main(int argc, char **argv)
 {
-	auto model = new Model(LossType::MSE);
+	auto model = new Model(LossType::CrossEntropy, 10.f);
 
-	int batch_size = 8;
+	int batch_size = 16;
 
-	// model->conv2d({batch_size, 1, 28, 28}, {4, 1, 3, 3});
-	// model->activation(ActivationType::Relu);
-	// model->conv2d({4, 4, 3, 3});
-	// model->activation(ActivationType::Relu);
-	// model->flatten();
-	// model->linear(16);
-	// model->activation(ActivationType::Relu);
-	// model->linear(16);
-	// model->activation(ActivationType::Relu);
-	// model->linear(10);
-	// model->activation(ActivationType::Sigmoid);
-
-	model->flatten({batch_size, 1, 28, 28});
-	model->linear(16);
+	model->conv2d({batch_size, 1, 28, 28}, {8, 1, 7, 7});
 	model->activation(ActivationType::Relu);
-	model->linear(16);
+	model->conv2d({8, 8, 5, 5});
+	model->activation(ActivationType::Relu);
+	model->conv2d({4, 8, 3, 3});
+	model->activation(ActivationType::Relu);
+	model->flatten();
+	model->linear(64);
 	model->activation(ActivationType::Relu);
 	model->linear(10);
-	model->activation(ActivationType::Sigmoid);
+	model->activation(ActivationType::Relu);
 	model->softmax();
 
 	mnist::train_mnist(model, batch_size, 5);
